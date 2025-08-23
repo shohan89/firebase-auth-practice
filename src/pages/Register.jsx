4e5bbb5f2454d6851c8d0582117342c174453074
shoh4n <maffin.killer007@gmail.com>
2025-08-23 13:54:30 +0600

    add Login & Registration form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,23 @@
 
 const Register = () => {
     return (
-        <div>
-            <h2>This is Register</h2>
+        <div className="w-[500px] mx-auto p-4 mt-6">
+            <h2 className="text-2xl font-bold text-center mb-6">Registration Form</h2>
+            <form className="w-full space-y-4 border p-4 shadow-lg rounded-lg">
+                <div>
+                    <label className="label">Name</label><br />
+                    <input type="text" name="name" className="input w-full" placeholder="Name" required />
+                </div>
+                <div>
+                    <label className="label">Email</label><br />
+                    <input type="text" name="email" className="input w-full" placeholder="Email" required />
+                </div>
+                <div>
+                    <label className="label">Password</label><br />
+                    <input type="password" name="password" className="input w-full" placeholder="password" required />
+                </div>
+                <button className="btn btn-primary w-full">Register</button>
+            </form>
         </div>
     );
 };
